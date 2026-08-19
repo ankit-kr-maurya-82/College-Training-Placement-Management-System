c3fd479ef77a860a218afef1ca351813e14f9fcf
ankit-kr-maurya <ankitkrmaurya82@gmail.com>
2026-08-19 21:31:50 +0530

    model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose, {Schema} from "mongoose"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true,
+            index:true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            index:true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        bio:{
+            type: String,
+            trim: true,
+            default: ""
+        },
+        avatar: {
+            type: String
+        },
+        password:{
+            type: String,
+            required: [true, "Password is required"]
+        },
+        role:{
+            type: String,
+            default: "user"
+        }
+    },
+    {
+        timestamps:true
+    })
+
+
+export const User = mongoose.model("User")
